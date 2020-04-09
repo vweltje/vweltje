5b4ad8e4e9e35596276ab80864267c0eaef84b1e
vweltje <vincentweltje@gmail.com>
2020-04-09 11:47:26 +0200

    Added base styling

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 
+import "./index.scss";
+
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
