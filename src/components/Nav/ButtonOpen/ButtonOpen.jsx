380af46539873ae839cdc334f3a8c6d2a8e1f722
vweltje <vincentweltje@gmail.com>
2020-04-09 11:55:42 +0200

    Use state hook in navigation

--- src/components/Nav/ButtonOpen/ButtonOpen.jsx
+++ src/components/Nav/ButtonOpen/ButtonOpen.jsx
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./ButtonOpen.scss";
 
-class ButtonOpen extends Component {
-  render() {
-    const { onButtonClick } = this.props;
-    return (
-      <button className="ButtonOpen" type="button" onClick={onButtonClick}>
-        <span className="ButtonOpen--Line first left" />
-        <span className="ButtonOpen--Line first right" />
-        <span className="ButtonOpen--Line second left" />
-        <span className="ButtonOpen--Line second right" />
-        <span className="ButtonOpen--Line third left" />
-        <span className="ButtonOpen--Line third right" />
-      </button>
-    );
-  }
-}
+const ButtonOpen = ({ onButtonClick }) => (
+  <button className="ButtonOpen" type="button" onClick={onButtonClick}>
+    <span className="ButtonOpen--Line first left" />
+    <span className="ButtonOpen--Line first right" />
+    <span className="ButtonOpen--Line second left" />
+    <span className="ButtonOpen--Line second right" />
+    <span className="ButtonOpen--Line third left" />
+    <span className="ButtonOpen--Line third right" />
+  </button>
+);
 
 export default ButtonOpen;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 
 import ButtonOpen from "./ButtonOpen/ButtonOpen";
@@ -10,85 +10,59 @@ import InstagramIcon from "../../svg/instagram.svg";
 
 import "./Nav.scss";
 
-class Nav extends Component {
-  constructor() {
-    super();
+const Nav = () => {
+  const [navActive, setNavActive] = useState();
 
-    this.state = {
-      navStatus: ""
-    };
-
-    this.close = this.close.bind(this);
-    this.open = this.open.bind(this);
-  }
-
-  close(e) {
-    e.stopPropagation();
-    this.setState({
-      navStatus: ""
-    });
-  }
-
-  open(e) {
-    e.stopPropagation();
-    this.setState({
-      navStatus: "active"
-    });
-  }
-
-  render() {
-    const { navStatus } = this.state;
-    return (
-      <>
-        <ButtonOpen onButtonClick={this.open} />
-        <nav className={`Navigation ${navStatus}`}>
-          <div className="Navigation--Container">
-            <ButtonClose onButtonClick={this.close} />
-            <Link to="/" className="Navigation--IconLink">
-              <Logo className="Navigation--Icon" />
-            </Link>
-            <ul className="Navigation--Links">
-              <li className="Navigation--Link">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="Navigation--Link">
-                <Link to="/work">Work</Link>
-              </li>
-              <li className="Navigation--Link">
-                <Link to="/photography">Photography</Link>
-              </li>
-              <li className="Navigation--Link">
-                <Link to="/about">About me</Link>
-              </li>
-              <li className="Navigation--Link">
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <ul className="Navigation--SocialIcons">
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/vweltje/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <LinkedinIcon />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/vweltje/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <InstagramIcon />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ButtonOpen onButtonClick={() => setNavActive(true)} />
+      <nav className={`Navigation ${navActive ? "active" : ""}`}>
+        <div className="Navigation--Container">
+          <ButtonClose onButtonClick={() => setNavActive(false)} />
+          <Link to="/" className="Navigation--IconLink">
+            <Logo className="Navigation--Icon" />
+          </Link>
+          <ul className="Navigation--Links">
+            <li className="Navigation--Link">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="Navigation--Link">
+              <Link to="/work">Work</Link>
+            </li>
+            <li className="Navigation--Link">
+              <Link to="/photography">Photography</Link>
+            </li>
+            <li className="Navigation--Link">
+              <Link to="/about">About me</Link>
+            </li>
+            <li className="Navigation--Link">
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <ul className="Navigation--SocialIcons">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/vweltje/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedinIcon />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/vweltje/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <InstagramIcon />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Nav;
