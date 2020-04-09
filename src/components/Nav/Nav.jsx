f248ef588197454cb065f08da1ce359ee34bbc97
vweltje <vincentweltje@gmail.com>
2020-04-09 15:37:15 +0200

    clone layout content on menu active

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "gatsby";
 
 import ButtonOpen from "./ButtonOpen/ButtonOpen";
@@ -10,59 +10,55 @@ import InstagramIcon from "../../svg/instagram.svg";
 
 import "./Nav.scss";
 
-const Nav = () => {
-  const [navActive, setNavActive] = useState();
-
-  return (
-    <>
-      <ButtonOpen onButtonClick={() => setNavActive(true)} />
-      <nav className={`Navigation ${navActive ? "active" : ""}`}>
-        <div className="Navigation--Container">
-          <ButtonClose onButtonClick={() => setNavActive(false)} />
-          <Link to="/" className="Navigation--IconLink">
-            <Logo className="Navigation--Icon" />
-          </Link>
-          <ul className="Navigation--Links">
-            <li className="Navigation--Link">
-              <Link to="/">Home</Link>
-            </li>
-            <li className="Navigation--Link">
-              <Link to="/work">Work</Link>
-            </li>
-            <li className="Navigation--Link">
-              <Link to="/photography">Photography</Link>
-            </li>
-            <li className="Navigation--Link">
-              <Link to="/about">About me</Link>
-            </li>
-            <li className="Navigation--Link">
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <ul className="Navigation--SocialIcons">
-            <li>
-              <a
-                href="https://www.linkedin.com/in/vweltje/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <LinkedinIcon />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.instagram.com/vweltje/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <InstagramIcon />
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-};
+const Nav = ({ navActive, setNavActive }) => (
+  <>
+    <ButtonOpen onButtonClick={() => setNavActive(true)} />
+    <nav className={`Navigation ${navActive ? "active" : ""}`}>
+      <div className="Navigation--Container">
+        <ButtonClose onButtonClick={() => setNavActive(false)} />
+        <Link to="/" className="Navigation--IconLink">
+          <Logo className="Navigation--Icon" />
+        </Link>
+        <ul className="Navigation--Links">
+          <li className="Navigation--Link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="Navigation--Link">
+            <Link to="/work">Work</Link>
+          </li>
+          <li className="Navigation--Link">
+            <Link to="/photography">Photography</Link>
+          </li>
+          <li className="Navigation--Link">
+            <Link to="/about">About me</Link>
+          </li>
+          <li className="Navigation--Link">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        <ul className="Navigation--SocialIcons">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/vweltje/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedinIcon />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/vweltje/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramIcon />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </>
+);
 
 export default Nav;
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Helmet from "react-helmet";
 import Nav from "../components/Nav/Nav";
 import config from "../../data/SiteConfig";
 
 import "./index.scss";
 
-export default class MainLayout extends React.Component {
-  render() {
-    const { children } = this.props;
-    return (
-      <div className="layout-container">
-        <Helmet>
-          <meta name="description" content={config.siteDescription} />
-          <html lang="en" />
-        </Helmet>
-        <Nav />
-        {children}
-      </div>
-    );
-  }
-}
+const SplitContent = ({ split, children }) => {
+  return (
+    <>
+      <main>{children}</main>
+      {split && <div className="Clone">{children}</div>}
+    </>
+  );
+};
+
+const MainLayout = ({ children }) => {
+  const [navActive, setNavActive] = useState(false);
+  return (
+    <>
+      <Helmet>
+        <meta name="description" content={config.siteDescription} />
+        <html lang="en" />
+      </Helmet>
+      <Nav navActive={navActive} setNavActive={setNavActive} />
+      <SplitContent split={navActive}>{children}</SplitContent>
+    </>
+  );
+};
+
+export default MainLayout;
