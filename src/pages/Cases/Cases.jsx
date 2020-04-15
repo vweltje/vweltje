02554d1a88e6faa4bc609fcb1510edb48507c1e1
vweltje <vincentweltje@gmail.com>
2020-04-15 14:27:58 +0200

    Added PageIntro ti cases page

--- src/pages/Cases/Cases.jsx
+++ src/pages/Cases/Cases.jsx
@@ -1,17 +1,19 @@
 import React from "react"
 import Helmet from "react-helmet"
 import Layout from "../../layout"
-import SimpleHeader from "../../components/SimpleHeader/SimpleHeader"
 import SEO from "../../components/SEO/SEO"
+import SimpleHeader from "../../components/SimpleHeader/SimpleHeader"
+import PageIntro from "../../components/PageIntro/PageIntro"
 import config from "../../../data/SiteConfig"
 
 const Cases = ({ data }) => {
-  console.log(data)
+  const pageData = { ...data?.case?.frontmatter, ...data?.case?.fields }
   return (
     <Layout>
       <Helmet title={config.siteTitle} />
       <SEO />
       <SimpleHeader />
+      <PageIntro title={pageData?.title} excerpt={pageData?.excerpt} />
     </Layout>
   )
 }
