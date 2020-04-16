d869cbd7cabebcf208260e898da2d2f0144391d9
vweltje <vincentweltje@gmail.com>
2020-04-16 19:18:14 +0200

    Created a breakpoint helper

---
+++ src/helpers/breakpointHelper.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react"
+import { throttle } from "lodash"
+
+const breakpoints = {
+  extraLarge: 1200,
+  large: 992,
+  medium: 768,
+  small: 576
+}
+
+const getCurrentBreakpoint = (width) => {
+  let breakpoint = ""
+  Object.keys(breakpoints).forEach((name) => {
+    breakpoint = width <= breakpoints[name] ? name : breakpoint
+  })
+  return breakpoint
+}
+
+const isDownBreakpoint = (breakpoint, width) => width <= breakpoints[breakpoint]
+
+const useBreakpoint = () => {
+  const [breakpoint, setBreakpoint] = useState(() =>
+    getCurrentBreakpoint(window.innerWidth)
+  )
+
+  useEffect(() => {
+    const calculateInnerWidth = throttle(() => {
+      setBreakpoint(getCurrentBreakpoint(window.innerWidth))
+    }, 200)
+    window.addEventListener("resize", calculateInnerWidth)
+    return () => window.removeEventListener("resize", calculateInnerWidth)
+  }, [])
+
+  return breakpoint
+}
+
+const downBreakpoint = (breakpoint) => {
+  const [isDown, setIsDown] = useState(() =>
+    isDownBreakpoint(breakpoint, window.innerWidth)
+  )
+
+  useEffect(() => {
+    const calculateInnerWidth = throttle(() => {
+      setIsDown(isDownBreakpoint(breakpoint, window.innerWidth))
+    }, 200)
+    window.addEventListener("resize", calculateInnerWidth)
+    return () => window.removeEventListener("resize", calculateInnerWidth)
+  }, [])
+
+  return isDown
+}
+
+export { useBreakpoint as default, downBreakpoint }
