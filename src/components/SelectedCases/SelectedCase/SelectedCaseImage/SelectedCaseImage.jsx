b91b1629ea59156a15f86d64ff280e9393ba0c21
vweltje <vincentweltje@gmail.com>
2020-04-16 19:20:31 +0200

    Disabled parralax effect on selected cases image on breakpoints down large

--- src/components/SelectedCases/SelectedCase/SelectedCaseImage/SelectedCaseImage.jsx
+++ src/components/SelectedCases/SelectedCase/SelectedCaseImage/SelectedCaseImage.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Parallax } from "react-scroll-parallax"
 import Image from "../../../Image/Image"
-
+import { downBreakpoint } from "../../../../helpers/breakpointHelper"
 import "./SelectedCaseImage.scss"
 
 const SelectedCaseImage = ({ image, backgroundAnimationAxis = "y" }) => {
@@ -15,11 +15,13 @@ const SelectedCaseImage = ({ image, backgroundAnimationAxis = "y" }) => {
         <Parallax
           className="Left"
           y={backgroundAnimationAxis === "y" ? [-7.5, 7.5] : [0, 0]}
+          disabled={downBreakpoint("large")}
         />
         <Parallax
           className="Right"
           y={backgroundAnimationAxis === "xy" ? [-7.5, 7.5] : [0, 0]}
           x={backgroundAnimationAxis === "xy" ? [-7.5, 0] : [0, 0]}
+          disabled={downBreakpoint("large")}
         />
       </div>
       <Image resolutions="small" lazy={false} src={image} alt={image} />
