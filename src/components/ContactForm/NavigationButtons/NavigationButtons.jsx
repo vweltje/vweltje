5c0853fadfaf6e0f39ba35dd36bab1c0eb952817
vweltje <vincentweltje@gmail.com>
2020-04-19 12:29:57 +0200

    Added basic behaviour to navigtion buttons in contactForm

--- src/components/ContactForm/NavigationButtons/NavigationButtons.jsx
+++ src/components/ContactForm/NavigationButtons/NavigationButtons.jsx
@@ -1,17 +1,39 @@
-import React from "react"
-
+import React, { useContext } from "react"
+import { store } from "../../../store"
 import "./NavigationButtons.scss"
 
-const NavigationButtons = () => (
-  <div className="ContactForm--NavigationButtons">
-    <button className="ContactForm--NavigationButton Prev" type="button">
-      Prev
-    </button>
-    /
-    <button className="ContactForm--NavigationButton Next" type="button">
-      Next
-    </button>
-  </div>
-)
+const NavigationButtons = () => {
+  const {
+    state: {
+      contactOverlay: { activeField, activeFieldValid }
+    },
+    dispatch
+  } = useContext(store)
+
+  return (
+    <div className="ContactForm--NavigationButtons">
+      {activeField > 1 && (
+        <>
+          <button
+            className="ContactForm--NavigationButton Prev"
+            type="button"
+            onClick={() => dispatch("contactOverlay--previousField")}
+          >
+            Prev
+          </button>
+          /
+        </>
+      )}
+      <button
+        className="ContactForm--NavigationButton Next"
+        type="button"
+        disabled={!activeFieldValid}
+        onClick={() => dispatch("contactOverlay--nextField")}
+      >
+        Next
+      </button>
+    </div>
+  )
+}
 
 export default NavigationButtons
