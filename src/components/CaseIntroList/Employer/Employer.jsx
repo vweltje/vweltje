7a35d922f3e54e1d153f0c647c49fd579e858d52
vweltje <vincentweltje@gmail.com>
2020-04-23 15:14:59 +0200

    Fixes issue with casesIntroList appearing when the it was a personalProject

--- src/components/CaseIntroList/Employer/Employer.jsx
+++ src/components/CaseIntroList/Employer/Employer.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import OddesseySolutionsIcon from "../../../svg/oddessey-solutions-icon.svg"
 import ThriveWebIcon from "../../../svg/thrive-web-icon.svg"
+import VWLogo from "../../../svg/vw-logo.svg"
 
 const employers = {
   oddesseySolutions: {
@@ -12,19 +13,33 @@ const employers = {
     url: "https://thriveweb.com.au/",
     icon: <ThriveWebIcon />,
     name: "Thrive Web"
+  },
+  personalProject: {
+    url: false,
+    icon: <VWLogo />,
+    name: "Freelance"
   }
 }
 
 const Employer = ({ name }) => (
-  <a
-    href={employers[name].url}
-    target="_blank"
-    rel="noopener noreferrer"
-    title={employers[name].name}
-  >
-    {employers[name].icon}
-    {employers[name].name}
-  </a>
+  <>
+    {employers[name].url ? (
+      <a
+        href={employers[name].url}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={employers[name].name}
+      >
+        {employers[name].icon}
+        {employers[name].name}
+      </a>
+    ) : (
+      <span>
+        {employers[name].icon}
+        {employers[name].name}
+      </span>
+    )}
+  </>
 )
 
 export default Employer
