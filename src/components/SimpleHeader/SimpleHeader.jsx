9c3524ff35b5a4a5d38cb7ca077f9ac77bc299b8
vweltje <vincentweltje@gmail.com>
2020-04-24 16:10:16 +0200

    Fixed mobile simple header background color when mobile menu is active

--- src/components/SimpleHeader/SimpleHeader.jsx
+++ src/components/SimpleHeader/SimpleHeader.jsx
@@ -1,17 +1,42 @@
-import React from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { Link } from "gatsby"
 import Container from "../Container/Container"
 import Logo from "../../svg/vw-logo.svg"
+import { downBreakpoint } from "../../helpers/breakpointHelper"
+import { store } from "../../store"
 import "./SimpleHeader.scss"
 
-const SimpleHeader = () => (
-  <header className="SimpleHeader">
-    <Container>
-      <Link to="/">
-        <Logo className="SimpleHeader--Logo" />
-      </Link>
-    </Container>
-  </header>
-)
+const SimpleHeader = () => {
+  const {
+    state: {
+      navigation: { active }
+    }
+  } = useContext(store)
+  const [isMobileNavigationAtive, setIsMobileNavigationAtive] = useState(false)
+  const changeBackgroundColor = downBreakpoint("large") && active
+
+  useEffect(() => {
+    if (changeBackgroundColor) {
+      setIsMobileNavigationAtive(true)
+    }
+    if (!changeBackgroundColor) {
+      setIsMobileNavigationAtive(false)
+    }
+  }, [changeBackgroundColor])
+
+  return (
+    <header
+      className={`SimpleHeader${
+        isMobileNavigationAtive ? " MobleMenuActive" : ""
+      }`}
+    >
+      <Container>
+        <Link to="/">
+          <Logo className="SimpleHeader--Logo" />
+        </Link>
+      </Container>
+    </header>
+  )
+}
 
 export default SimpleHeader
